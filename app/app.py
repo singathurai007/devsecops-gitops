7e31d51f6bcb7592a3bbdf8cf6fa2b60c30a5c4c
singathurai007 <cmsingathurai@gmail.com>
2026-09-01 17:34:38 +0000

    Update application to v3

--- app/app.py
+++ app/app.py
@@ -2,13 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "DevSecOps boyss daa  Application is Running!Hello from devsecops v2"
+    return "DevSecOps boys daa - Application is Running! Hello from devsecops v3"
+
 
 @app.route("/health")
 def health():
     return "OK"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
